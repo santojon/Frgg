8d03e0443711639168ba014fdc85cf0535edff00
Jonathan Santos <santojon5@gmail.com>
2016-09-13 16:31:20 -0300

    update

--- frgg.js
+++ frgg.js
@@ -28,11 +28,22 @@
      * Remove Frigga scripts after all is done
      * @param script: a list with all scripts in page to analyse
      */
-    cleanup = function(script) {
+    cleanup = function() {
+        // remove frigga references
+        var script = document.getElementsByTagName('script');
         for(i = 0; i < script.length; i++) {
             if(script[i].type == 'text/frigga') {
                 parent = script[i].parentNode;
                 parent.removeChild(script[i]);
+            }
+        }
+
+        // remove empty divs
+        var div = document.getElementsByTagName('div');
+        for(i = 0; i < div.length; i++) {
+            if (div[i].innerHTML == '') {
+                parent = div[i].parentNode;
+                parent.removeChild(div[i]);
             }
         }
     }
@@ -43,24 +54,30 @@
     compileTemplates = function() {
         var script = document.getElementsByTagName('script');
         var i, src = [], elem;
+
+        // find for frigga scripts
         for(i = 0; i < script.length; i++) {
             if(script[i].type == 'text/frigga') {
+                // create new element
                 elem = document.createElement('div');
                 parent = script[i].parentNode;
                 parent.insertBefore(elem, script[i]);
 
+                // import frigga script into
                 if(script[i].src) {
                     elem.innerHTML = load(script[i].src);
                 } else {
                     elem.innerHTML = script[i].innerHTML;
                 }
 
+                // put it all raw int html file in right position
                 while (elem.firstChild) {
-                    elem.parentNode.insertBefore(elem.firstChild,elem);
+                    elem.parentNode.insertBefore(elem.firstChild, elem);
                 }
             }
         }
-        cleanup(script);
+        // clean that mess
+        cleanup();
     }
 
     // compile
